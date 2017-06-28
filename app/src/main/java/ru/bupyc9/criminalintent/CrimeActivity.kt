ec9d1e1a3ce35fcc7cf11d2309e96dc7e4bef90c
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-06-28 23:30:28 +0300

    Добавление фрагмента в активность

--- app/src/main/java/ru/bupyc9/criminalintent/CrimeActivity.kt
+++ app/src/main/java/ru/bupyc9/criminalintent/CrimeActivity.kt
@@ -1,12 +1,23 @@
 package ru.bupyc9.criminalintent
 
 import android.os.Bundle
-import android.support.v4.app.FragmentActivity
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import ru.bupyc9.criminalintent.ui.CrimeFragment
 
-class CrimeActivity : FragmentActivity() {
+class CrimeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crime)
+
+        addFragment(CrimeFragment.newInstance())
+    }
+
+    fun addFragment(fragment: Fragment) {
+        supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
     }
 }
