a0996c2860727c373788c1e3fb664fbeeeee0040
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-07-01 23:25:46 +0300

    Добавление интерфейса Parcelable для модели Crime

--- app/src/main/java/ru/bupyc9/criminalintent/models/Crime.kt
+++ app/src/main/java/ru/bupyc9/criminalintent/models/Crime.kt
@@ -1,5 +1,7 @@
 package ru.bupyc9.criminalintent.models
 
+import android.os.Parcel
+import android.os.Parcelable
 import java.util.*
 
 data class Crime(
@@ -7,5 +9,27 @@ data class Crime(
         var title: String,
         var date: Date,
         var solved: Boolean
-) {
+) : Parcelable {
+    companion object {
+        @JvmField val CREATOR: Parcelable.Creator<Crime> = object : Parcelable.Creator<Crime> {
+            override fun createFromParcel(source: Parcel): Crime = Crime(source)
+            override fun newArray(size: Int): Array<Crime?> = arrayOfNulls(size)
+        }
+    }
+
+    constructor(source: Parcel) : this(
+            source.readInt(),
+            source.readString(),
+            source.readSerializable() as Date,
+            1 == source.readInt()
+    )
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeInt(id)
+        dest.writeString(title)
+        dest.writeSerializable(date)
+        dest.writeInt((if (solved) 1 else 0))
+    }
 }
