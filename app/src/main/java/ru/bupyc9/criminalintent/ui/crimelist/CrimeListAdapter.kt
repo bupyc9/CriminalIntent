ccfa4f70f4551f714dbbc73e6cb9619f9a435689
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-07-02 11:22:29 +0300

    Установка listener onclick в адаптер

--- app/src/main/java/ru/bupyc9/criminalintent/ui/crimelist/CrimeListAdapter.kt
+++ app/src/main/java/ru/bupyc9/criminalintent/ui/crimelist/CrimeListAdapter.kt
@@ -8,7 +8,9 @@ import ru.bupyc9.criminalintent.R
 import ru.bupyc9.criminalintent.models.Crime
 import kotlinx.android.synthetic.main.item_crime.view.*
 
-class CrimeListAdapter(private val mItems: List<Crime>) : RecyclerView.Adapter<CrimeListAdapter.ViewHolder>() {
+class CrimeListAdapter(private val mItems: MutableList<Crime>) : RecyclerView.Adapter<CrimeListAdapter.ViewHolder>() {
+    private var mOnClickListener: ((View?, Crime) -> Unit)? = null
+
     override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
         val viewHolder = holder as ViewHolder
         val crime = getItem(position)
@@ -31,11 +33,26 @@ class CrimeListAdapter(private val mItems: List<Crime>) : RecyclerView.Adapter<C
         return mItems.size
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun setOnClickListener(listener: (View?, Crime) -> Unit) {
+        mOnClickListener = listener
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        private lateinit var mCrime: Crime
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
         fun bind(crime: Crime) {
+            mCrime = crime
             itemView.crime_title.text = crime.title
             itemView.crime_date.text = crime.date.toString()
             itemView.crime_solved.isChecked = crime.solved
+        }
+
+        override fun onClick(view: View?) {
+            mOnClickListener?.invoke(view, mCrime)
         }
     }
 }
