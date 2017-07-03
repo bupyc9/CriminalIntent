13319c31d54c4ea458e8415d836659bc1d572142
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-07-03 23:06:41 +0300

    Меню параметров в фрагменте "CrimeListFragment"

--- app/src/main/java/ru/bupyc9/criminalintent/ui/crimelist/CrimeListFragment.kt
+++ app/src/main/java/ru/bupyc9/criminalintent/ui/crimelist/CrimeListFragment.kt
@@ -3,13 +3,12 @@ package ru.bupyc9.criminalintent.ui.crimelist
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import ru.bupyc9.criminalintent.R
 import kotlinx.android.synthetic.main.fragment_crime_list.*
 import ru.bupyc9.criminalintent.CrimeActivity
 import ru.bupyc9.criminalintent.models.Crime
+import ru.bupyc9.criminalintent.ui.CrimeLab
 import ru.bupyc9.criminalintent.ui.crime.CrimePagerFragment
 import java.util.*
 import kotlin.collections.ArrayList
@@ -28,6 +27,11 @@ class CrimeListFragment : Fragment() {
 
     private lateinit var mAdapter: CrimeListAdapter
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.fragment_crime_list, container, false)
     }
@@ -37,21 +41,42 @@ class CrimeListFragment : Fragment() {
         crime_list.layoutManager = LinearLayoutManager(activity)
         crime_list.setHasFixedSize(true)
 
-        val crimes: MutableList<Crime> = mutableListOf()
-
-        (1..100).mapTo(crimes) {
-            Crime(
-                    it,
-                    "Title $it",
-                    Date(),
-                    false
-            )
-        }
-
         val activity = activity as CrimeActivity
 
-        mAdapter = CrimeListAdapter(crimes)
-        mAdapter.setOnClickListener { _, crime -> activity.addFragment(CrimePagerFragment.newInstance(crimes as ArrayList<Crime>, crimes.indexOf(crime)), true) }
+        mAdapter = CrimeListAdapter(CrimeLab.instance.crimes)
+        mAdapter.setOnClickListener { _, crime ->
+            activity.addFragment(
+                    CrimePagerFragment.newInstance(
+                            CrimeLab.instance.crimes as ArrayList<Crime>,
+                            CrimeLab.instance.crimes.indexOf(crime)
+                    ),
+                    true
+            )
+        }
         crime_list.adapter = mAdapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater?.inflate(R.menu.fragment_crime_list, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?) = when {
+        item?.itemId == R.id.menu_item_new_crime -> optionItemNewCrime()
+        else -> super.onOptionsItemSelected(item)
+    }
+
+    private fun optionItemNewCrime(): Boolean {
+        val crime = Crime(0, "", Date(), false)
+        CrimeLab.instance.crimes.add(crime)
+        val fragment = CrimePagerFragment.newInstance(
+                CrimeLab.instance.crimes as ArrayList<Crime>,
+                CrimeLab.instance.crimes.indexOf(crime)
+        )
+        val activity = activity as CrimeActivity
+        activity.addFragment(fragment, true)
+
+        return true
     }
 }
