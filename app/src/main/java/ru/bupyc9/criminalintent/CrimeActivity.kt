ed96e97e7ce401a0727b746fd581edc3d0f7a8dc
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-07-04 22:22:14 +0300

    Вывод кнопки "назад", на фрагменте "CrimeFragment"

--- app/src/main/java/ru/bupyc9/criminalintent/CrimeActivity.kt
+++ app/src/main/java/ru/bupyc9/criminalintent/CrimeActivity.kt
@@ -3,6 +3,7 @@ package ru.bupyc9.criminalintent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import ru.bupyc9.criminalintent.ui.crimelist.CrimeListFragment
 
 class CrimeActivity : AppCompatActivity() {
@@ -12,6 +13,12 @@ class CrimeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_fragment)
 
         addFragment(CrimeListFragment.newInstance())
+
+        supportFragmentManager.addOnBackStackChangedListener {
+            val show = supportFragmentManager.backStackEntryCount > 0
+            supportActionBar?.setHomeButtonEnabled(show)
+            supportActionBar?.setDisplayHomeAsUpEnabled(show)
+        }
     }
 
     fun addFragment(fragment: Fragment, addToBackStack: Boolean = false) {
@@ -24,5 +31,13 @@ class CrimeActivity : AppCompatActivity() {
         }
 
         transaction.commit()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean = when(item?.itemId) {
+        android.R.id.home -> {
+            supportFragmentManager.popBackStack()
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 }
